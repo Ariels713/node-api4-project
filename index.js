14e8f723ab3144147fd4b0c8cd71ff9a8279ec4c
Ariel_Rodriguez <32606708+Ariels713@users.noreply.github.com>
2020-06-11 21:53:07 -0400

    Ariel-Rodriguez

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const server = express()
+const port = process.env.port || 8050
+// const morgan = require('morgan')
+const router = require('./router/router')
+
+
+server.use(express.json())
+// server.use(morgan('dev'))
+
+server.use(express.urlencoded({ extended: true }))
+server.use('/api/posts', router)
+
+server.listen(port, () => console.log('Sever Running on Port 8050'))
